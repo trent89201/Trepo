7965270c4437a8b301a9ccf67fe61dd4a451f3d4
trent89201 <143125649+trent89201@users.noreply.github.com>
2023-08-30 11:42:49 -0700

    Create ExamplePython.py
    Added a job list

---
+++ ExamplePython.py
@@ -0,0 +1,14 @@
+a = "String 1"
+b = "String 2"
+print("a =" + a + " and b=" + b )
+
+x = "String 3"
+y = "String 4"
+print (f"x = {x} and y = {y}")
+
+print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
